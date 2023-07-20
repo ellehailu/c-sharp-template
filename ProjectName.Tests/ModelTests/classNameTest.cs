d781cda456982f7b06e06e1a0ca6671c85d9b905
ellehailu <hailu.elle@gmail.com>
2023-07-20 16:35:49 -0700

    added boilerplate code to files

---
+++ ProjectName.Tests/ModelTests/classNameTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ProjectName.Tests
+{
+    [TestClass]
+
+    public class ClassNameTests
+    {
+
+        [TestMethod]
+
+    }
+}
---
+++ ProjectName/Controllers/ClassController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+// using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+    public class ClassController : Controller
+    {
+
+    }
+}
